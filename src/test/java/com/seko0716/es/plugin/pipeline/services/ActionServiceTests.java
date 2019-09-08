93dc87b37176cd83597d25a9e5b99490b01330f1
Sergey34 <Sergey3440@gmail.com>
2019-09-08 14:04:12 +0400

    rename class to change test group

--- src/test/java/com/seko0716/es/plugin/pipeline/services/ActionServiceTests.java
+++ src/test/java/com/seko0716/es/plugin/pipeline/services/ActionServiceTests.java
@@ -4,7 +4,7 @@ import com.seko0716.es.plugin.pipeline.actions.Action;
 import org.elasticsearch.test.ESTestCase;
 import org.junit.Assert;
 
-public class ActionServiceIT extends ESTestCase {
+public class ActionServiceTests extends ESTestCase {
 
     public void testLoadActions() {
         String aClass = "com.seko0716.es.plugin.pipeline.actions.filter.RandomFilterErr";
